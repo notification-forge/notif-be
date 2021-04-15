a73b321d9954ba32c0424d2e893407161b574be6
kaiyanghuan <kaiyanghuan@gmail.com>
2021-04-15 11:02:20 +0800

    update spring security version

--- messaging-service/build.gradle.kts
+++ messaging-service/build.gradle.kts
@@ -15,11 +15,11 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation")
 
-    implementation("org.springframework.security:spring-security-ldap:5.4.1")
-    implementation("org.springframework.security:spring-security-core:5.4.1")
-    implementation("org.springframework.security:spring-security-crypto:5.4.1")
-    implementation("org.springframework.security:spring-security-web:5.4.1")
-    implementation("org.springframework.security:spring-security-config:5.4.1")
+    implementation("org.springframework.security:spring-security-ldap:5.4.6")
+    implementation("org.springframework.security:spring-security-core:5.4.6")
+    implementation("org.springframework.security:spring-security-crypto:5.4.6")
+    implementation("org.springframework.security:spring-security-web:5.4.6")
+    implementation("org.springframework.security:spring-security-config:5.4.6")
 
     implementation("io.jsonwebtoken:jjwt-api:0.11.1")
     implementation("io.jsonwebtoken:jjwt-impl:0.11.1")
