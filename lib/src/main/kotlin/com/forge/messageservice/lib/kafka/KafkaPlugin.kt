433ad202d5ffb6b73cab91a3340630d16fc7c469
Cuong Nguyen <cuongnc1112@gmail.com>
2021-04-25 15:25:07 +0800

    Invoke constructor with param using reflection

--- lib/src/main/kotlin/com/forge/messageservice/lib/kafka/KafkaPlugin.kt
+++ lib/src/main/kotlin/com/forge/messageservice/lib/kafka/KafkaPlugin.kt
@@ -1,15 +1,15 @@
 package com.forge.messageservice.lib.kafka
 
-    import com.alphamail.plugin.api.AlphamailPlugin
+import com.alphamail.plugin.api.AlphamailPlugin
 
 
 class KafkaPlugin(private val cfg: KafkaConfiguration) : AlphamailPlugin {
 
-    override fun beforeSend(): Any? {
+    override fun beforeSend(): Any {
         return "This is kafka plugin before sending, for ${cfg.kafkaServer}"
     }
 
-    override fun afterSend(): Any? {
+    override fun afterSend(): Any {
         return "This is kafka plugin after sending"
     }
 }
--- messaging-service/src/test/kotlin/main.kt
+++ messaging-service/src/test/kotlin/main.kt
@@ -1,21 +1,35 @@
 import com.alphamail.plugin.api.AlphamailPlugin
+import com.alphamail.plugin.api.PluginConfiguration
+import com.forge.messageservice.lib.kafka.KafkaConfiguration
 import java.net.URL
 import java.net.URLClassLoader
 import kotlin.jvm.internal.Reflection
-import kotlin.reflect.full.createInstance
+import kotlin.reflect.KClass
 
 class Loader {
 
     fun load(uri: String): Any? {
         val child = URLClassLoader(arrayOf(URL(uri)), this.javaClass.classLoader)
-        val classToLoad = Class.forName("me.cjavellana.lib.SomeLib", true, child)
+        val classToLoad = Class.forName("com.forge.messageservice.lib.kafka.KafkaPlugin", true, child)
         val kClass = Reflection.createKotlinClass(classToLoad)
-
-        val instance = kClass.createInstance() as AlphamailPlugin
-        return instance.beforeSend()
+        val config = KafkaConfiguration().apply {
+            kafkaServer = "bla blo"
+        }
+        val instance = kClass.createInstance(config) as AlphamailPlugin
+        println(instance.beforeSend())
+        return instance
     }
 }
 
+//Decorator to call create plugin instance with config
+fun <T : Any> KClass<T>.createInstance(config: PluginConfiguration): T {
+//    Can use filter here if you have more than 1 constructor
+    val noArgsConstructor = constructors.first()
+//    If decided every plugin doesn't have other param, then this is good enough
+    val params = noArgsConstructor.parameters.associateWith { config }
+//    Invoke constructor with param
+    return noArgsConstructor.callBy(params)
+}
 
 fun main(args: Array<String>) {
     val lib = "file:./lib/build/libs/lib-1.0.jar"
