915d4bdda3493b43476ec68c4c4adcef5006528a
Cuong Nguyen <cuongnc1112@gmail.com>
2021-04-27 22:32:34 +0800

    Adding Altair

--- messaging-service/build.gradle.kts
+++ messaging-service/build.gradle.kts
@@ -31,6 +31,7 @@ dependencies {
     implementation("com.graphql-java-kickstart:graphql-spring-boot-starter:11.0.0")
     implementation("com.graphql-java-kickstart:playground-spring-boot-starter:11.0.0")
     implementation("com.graphql-java-kickstart:voyager-spring-boot-starter:11.0.0")
+    implementation("com.graphql-java-kickstart:altair-spring-boot-starter:11.0.0")
     implementation("com.graphql-java:graphql-java-extended-scalars:1.0")
 
     implementation("org.hibernate.validator:hibernate-validator")
