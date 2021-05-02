b198b13fa01a6e5490c4d1228409db7d8a6e1170
Christian Javellana <cjjavellana@gmail.com>
2021-05-02 15:55:15 +0800

    Moved logging deps to buildSrc - common for all.

--- lib/src/main/kotlin/com/forge/messageservice/lib/kafka/KafkaConfiguration.kt
+++ lib/src/main/kotlin/com/forge/messageservice/lib/kafka/KafkaConfiguration.kt
@@ -3,9 +3,10 @@ package com.forge.messageservice.lib.kafka
 import com.alphamail.plugin.api.PluginConfiguration
 
 class KafkaConfiguration : PluginConfiguration {
+    var bootStrapAddress: String = ""
+    var kafkaTopic: String = ""
     var kafkaServer: String? = null
     var kafkaGroupId: String? = null
     var kafkaSslKeyPassword: String? = null
     var kafkaSslEnable: Boolean? = null
-    var kafkaTopic: String? = null
 }
--- lib/src/main/kotlin/com/forge/messageservice/lib/kafka/KafkaPlugin.kt
+++ lib/src/main/kotlin/com/forge/messageservice/lib/kafka/KafkaPlugin.kt
@@ -1,18 +1,55 @@
 package com.forge.messageservice.lib.kafka
 
 import com.alphamail.plugin.api.AlphamailPlugin
+import mu.KotlinLogging
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.common.serialization.StringSerializer
+import org.springframework.kafka.core.DefaultKafkaProducerFactory
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.support.SendResult
+import org.springframework.util.concurrent.ListenableFutureCallback
 
 /**
  * Kafka plugin drops a message to a kafka topic indicating that a message has been
  * sent.
  *
- * Notifications from this plugin can be used by tenants to update the status of their
- * email dispatches.
+ * This class acts as a wrapper and delegates the actual message sending to [KafkaMessageProducer] to that
+ * unit test can be written for [KafkaMessageProducer]
  */
-class KafkaPlugin(private val cfg: KafkaConfiguration) : AlphamailPlugin {
+class KafkaPlugin(cfg: KafkaConfiguration) : AlphamailPlugin {
+
+    private val logger = KotlinLogging.logger {}
+
+    private val kafkaTemplate: KafkaTemplate<String, String>
+    private val msgProducer: KafkaMessageProducer
+
+    private val handler = object : ListenableFutureCallback<SendResult<String, String>> {
+
+        override fun onSuccess(result: SendResult<String, String>?) {
+            logger.info { "Kafka message sent ${result?.recordMetadata?.offset()}" }
+        }
+
+        override fun onFailure(ex: Throwable) {
+            logger.error { ex }
+        }
+    }
+
+    init {
+        val defaultKafkaProducerFactory = DefaultKafkaProducerFactory<String, String>(
+            mapOf(
+                ProducerConfig.BOOTSTRAP_SERVERS_CONFIG to cfg.bootStrapAddress,
+                ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG to StringSerializer::javaClass,
+                ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG to StringSerializer::javaClass
+            )
+        )
+        kafkaTemplate = KafkaTemplate(defaultKafkaProducerFactory)
+        msgProducer = KafkaMessageProducer(cfg, kafkaTemplate, handler)
+    }
 
     override fun execute(): Any? {
-        TODO("Not yet implemented")
+        // TODO: Update `execute()` signature to pass the message to be sent
+        msgProducer.send("")
+        return null
     }
 
     override fun type(): AlphamailPlugin.PluginType {
