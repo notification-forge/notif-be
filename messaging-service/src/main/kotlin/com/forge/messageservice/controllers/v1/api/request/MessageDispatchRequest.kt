038eef1b9aaf6374b1ed63788daac3b05bbca37e
Christian Javellana <cjjavellana@gmail.com>
2021-05-05 05:29:35 +0800

    Enhanced request model for message dispatching request

---
+++ messaging-service/src/main/kotlin/com/forge/messageservice/controllers/v1/api/request/MessageDispatchRequest.kt
@@ -0,0 +1,9 @@
+package com.forge.messageservice.controllers.v1.api.request
+
+import com.forge.messageservice.common.messaging.Recipients
+
+data class MessageDispatchRequest(
+    val recipients: Recipients,
+    val subject: String,
+    val parameters: Map<String, Any>
+)
