96a25b279068ba2cb848281e2b0d229a45665f9a
Christian Javellana <cjjavellana@gmail.com>
2021-05-04 16:55:45 +0800

    Make Cuong happy - Fix build failure :)

--- messaging-service/src/test/kotlin/features/common/Common.kt
+++ messaging-service/src/test/kotlin/features/common/Common.kt
@@ -1,31 +1,42 @@
-//package features.common
-//
-//import features.stepdefs.CommonStepDefs
-//import okhttp3.OkHttpClient
-//import okhttp3.Request
-//import okhttp3.RequestBody
-//import okhttp3.Response
-//
-//val client = OkHttpClient()
-//
-//fun post(user: TestUser, url: String, requestBody: RequestBody, extraHeaders: Map<String, String> = emptyMap()): Response {
-//    val request = Request.Builder().post(requestBody)
-//        .addHeader("Authorization", "Bearer ${user.token}")
-//        .addHeader("Content-Type", "application/json")
-//        .url(url)
-//        .build()
-//    return client.newCall(requestWithExtraHeaders(request, extraHeaders)).execute()
-//}
-//
-//private fun requestWithExtraHeaders(request: Request, extraHeaders: Map<String, String> = emptyMap()): Request {
-//    if (extraHeaders.isNotEmpty()){
-//        var newBuilder = request.newBuilder()
-//
-//        for (m in extraHeaders){
-//            newBuilder = newBuilder.addHeader(m.key, m.value)
-//        }
-//
-//        return newBuilder.build()
-//    }
-//    return request
-//}
+package features.common
+
+import mu.KotlinLogging
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody
+import okhttp3.Response
+
+private val LOGGER = KotlinLogging.logger {}
+val client = OkHttpClient()
+
+fun post(
+    user: TestUser,
+    url: String,
+    requestBody: RequestBody,
+    extraHeaders: Map<String, String> = emptyMap()
+): Response {
+
+    if (LOGGER.isDebugEnabled) {
+        LOGGER.debug { "Calling $url using ${user.username} token ${user.token}" }
+    }
+
+    val request = Request.Builder().post(requestBody)
+        .addHeader("Authorization", "Bearer ${user.token}")
+        .addHeader("Content-Type", "application/json")
+        .url(url)
+        .build()
+    return client.newCall(requestWithExtraHeaders(request, extraHeaders)).execute()
+}
+
+private fun requestWithExtraHeaders(request: Request, extraHeaders: Map<String, String> = emptyMap()): Request {
+    if (extraHeaders.isNotEmpty()) {
+        var newBuilder = request.newBuilder()
+
+        for (m in extraHeaders) {
+            newBuilder = newBuilder.addHeader(m.key, m.value)
+        }
+
+        return newBuilder.build()
+    }
+    return request
+}
--- messaging-service/src/test/resources/1_test.feature
+++
@@ -1,7 +0,0 @@
-#lang: en
-Feature: 1 test
-
-  Scenario: User creates a new template
-    Given given test
-    When when test
-    Then then test
